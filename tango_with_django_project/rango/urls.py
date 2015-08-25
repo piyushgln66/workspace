a7ed94e4845be1e7a49f23c5a1026c86e434e1ab
piyushgln66 <piyushgln66@gmail.com>
2015-08-25 15:48:54 +0800

    commit

---
+++ tango_with_django_project/rango/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+from rango import views
+
+urlpatterns = patterns('',
+	url(r'^$', views.index, name='index'),
+	url(r'^add_category/$', views.add_category, name='add_category'),
+	url(r'^add_page/(?P<category_name_slug>[\w\-]+)/$', views.add_page, name='add_page'), 
+	url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),)
