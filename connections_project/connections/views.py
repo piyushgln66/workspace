912d9db295fe30bd8ae93b6309609ec80cece847
piyushgln66 <piyushgln66@gmail.com>
2015-09-01 19:04:26 +0800

    commit

--- connections_project/connections/views.py
+++ connections_project/connections/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 
-def index(request):
-    # Query the database for a list of ALL categories currently stored.
-    # Order the categories by no. likes in descending order.
-    # Retrieve the top 5 only - or all if less than 5.
-    # Place the list in our context_dict dictionary which will be passed to the template engine
 
-    # Render the response and send it back!
-    return render(request, 'connections/index.html')
+
+webAppTitle="MySite"
+
+def index(request):
+    context_dict = {'webAppTitle': webAppTitle, 'title':webAppTitle}
+    return render(request, 'connections/index.html', context_dict)
 
 
